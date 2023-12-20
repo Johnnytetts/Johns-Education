ef23761ebcd7c16f072d99813abaca10c8fd0b81
John <127613375+Johnnytetts@users.noreply.github.com>
2023-12-20 14:39:33 -0600

    try to sort numbers in order but script not working anymore when adding new sortednumbers function

---
+++ Cas Powershell Homework/Session 3 Bonus Sorting.ps1
@@ -0,0 +1,21 @@
+# Make array to store random numbers 0-200
+$randomNumbers = @()
+
+# Generate 10 random numbers
+for ($i = 0; $i -lt 10; $i++) 
+{$randomNumbers += Get-Random -Minimum 0 -Maximum 200}
+
+# Sort the numbers in order
+{$sortedNumbers = $randomNumbers | Sort-Object}
+
+# Sort through each number generated and add to the array
+foreach ($number in $sortedNumbers) {
+    # Check if number is >100 and write a message
+    if ($number -gt 100) {
+        Write-Host "$number is over 100"
+    }
+    # Check if number is <100 and write a message
+    else {
+        Write-Host "$number is under 100"
+    }
+}
