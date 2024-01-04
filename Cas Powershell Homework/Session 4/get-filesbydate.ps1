b953e0e54662eb4658b2a8b2c5596bd448504b46
John <jtetterton62@gmail.com>
2024-01-04 12:06:44 -0500

    add day range and if statement

--- Cas Powershell Homework/Session 4/get-filesbydate.ps1
+++ Cas Powershell Homework/Session 4/get-filesbydate.ps1
@@ -3,6 +3,8 @@ function Get-FilesByDate(){
         [Parameter(Mandatory=$true)]
         [ValidateSet('1','2','3','4','5','6','7','8','9','10','11','12')]
         [int]$month,
+        [Parameter(Mandatory=$false)]
+        [ValidateRange(1,31)]
         [int]$day = (get-date).Day,
         [int]$year = (get-date).Year,
         [Parameter(ValueFromPipeline=$true)]
@@ -11,6 +13,12 @@ function Get-FilesByDate(){
     process {
         Get-ChildItem -Path $path -Recurse |
         Where-Object {
+            if ($day) {
+                $_.LastWriteTime.month -eq $month -and $_.LastWriteTime.Year -eq $year -and $_.LastWriteTime.day -eq $day
+            }
+            else {
+                $_.LastWriteTime.month -eq $month -and $_.LastWriteTime.year -eq $year
+            }
         $_.lastwritetime.month -eq $month -AND $_.lastwritetime.year -eq $year 
         }
     }
