c8f55938f756afcd9cf6a3067c9a7c7816deec80
John <127613375+Johnnytetts@users.noreply.github.com>
2024-01-24 17:55:44 -0500

    add ram availability
    i think i need to include the thresholds into one process block. Going to commit for now to be able to revert if what I change dosent work.

--- Powershell Scripting Projects/Performance Monitor.ps1
+++ Powershell Scripting Projects/Performance Monitor.ps1
@@ -1,3 +1,4 @@
+#This function is used to check the current CPU Usage. Alter reporting time by editing the $threshold value.
 function get-cpuusage {
         $cpupwr = get-counter '\processor(_total)\% processor time'
         $cpupwr = $cpupwr.countersamples[0].cookedvalue
@@ -12,4 +13,19 @@ if ($cpupwr -gt $threshold) {
 }
 
 start-sleep -seconds 60
+}
+
+#This function is used to check the current RAM usage. Alter reporting time by editing the $threshold value.
+function get-ramusage {
+        $rampwr = get-counter '\memory\available mbytes'
+        $rampwr = $rampwr.countersamples[0].cookedvalue
+        return $rampwr
+    }
+
+while($true) {
+    $rampwr = get-ramusage
+    $threshold = 99
+if ($rampwr -lt $threshold) {
+    Write-Verbose "RAM usage is above current threshold of $threshold MB" -Verbose
+}
 }
