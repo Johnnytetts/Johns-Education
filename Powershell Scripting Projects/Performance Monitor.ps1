43923a405a4e0ff23e60e83ec002e4466cdd1a1a
John <127613375+Johnnytetts@users.noreply.github.com>
2024-01-26 11:03:22 -0500

    Fix Ram function
    Removed infinite loop with CPU function and added ram inside the loop to ensure script captures both. Added threshold for both functions and calculated ram usage with a percentage divided by the total amount of ram available.

--- Powershell Scripting Projects/Performance Monitor.ps1
+++ Powershell Scripting Projects/Performance Monitor.ps1
@@ -1,31 +1,36 @@
 #This function is used to check the current CPU Usage. Alter reporting time by editing the $threshold value.
 function get-cpuusage {
-        $cpupwr = get-counter '\processor(_total)\% processor time'
+    $cpupwr = get-counter '\processor(_total)\% processor time'
         $cpupwr = $cpupwr.countersamples[0].cookedvalue
-        return $cpupwr
-    }
+       return $cpupwr
+   }
+
+   #This function is used to check the percentage of available RAM. Alter reporting time by editing the $threshold value.
+function get-ramusage {
+    $totalram = get-ciminstance -classname cim_computersystem | select-sbject -expandproperty totalphysicalmemory
+    $totalrammb = $totalram / 1MB
+    $availableram = get-counter '\memory\available mbytes'
+    $availablerammb = $availableram.countersamples[0].cookedvalue
+    $usedrammb = $totalrammb - $availablerammb
+    return $usedrammb
+}
 
 while ($true) {
     $cpupwr = get-cpuusage
     $threshold = 5
 if ($cpupwr -gt $threshold) {
-    Write-Verbose "CPU usage is above current threshold of $threshold %" -Verbose
+    write-verbose "CPU usage is above current threshold of $threshold %" -verbose
+}
+
+$rampwr = get-ramusage
+$totalram = get-ciminstance -ClassName CIM_ComputerSystem | select-object -expandproperty totalphysicalmemory
+$totalrammb = $totalram / 1MB
+$threshold = 0.20 * $totalrammb  # This calculates a percentage of total ram gathered from the $totalram variable. Edit the 0.20 value to change the percentage.
+if ($rampwr -gt $threshold) {
+    write-verbose "RAM usage is above current threshold of $threshold MB" -verbose
 }
 
 start-sleep -seconds 60
 }
 
-#This function is used to check the current RAM usage. Alter reporting time by editing the $threshold value.
-function get-ramusage {
-        $rampwr = get-counter '\memory\available mbytes'
-        $rampwr = $rampwr.countersamples[0].cookedvalue
-        return $rampwr
-    }
 
-while($true) {
-    $rampwr = get-ramusage
-    $threshold = 99
-if ($rampwr -lt $threshold) {
-    Write-Verbose "RAM usage is above current threshold of $threshold MB" -Verbose
-}
-}
